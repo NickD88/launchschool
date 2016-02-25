ecb7ee9bdc42d4369a4056f08c9a284c7c39a97c
Nick Dectis <ndectis@gmail.com>
2016-02-24 23:16:31 -0500

    refactor twenty-one

--- 101_foundations/lesson_4/twenty_one.rb
+++ 101_foundations/lesson_4/twenty_one.rb
@@ -12,36 +12,24 @@ end
 
 def display_heading(score_board)
   clear_screen
-  puts <<-EOF
----BLACKJACK---
+  puts <<~EOF
+    ---BLACKJACK---
 
-First to 5 wins the match
+    First to 5 wins the match
 
-Rounds Won: Player: #{score_board[:player]}  Dealer: #{score_board[:dealer]}
-
-  EOF
-end
-
-def display_start_game(player, dealer, score_board)
-  display_heading(score_board)
-  puts <<-EOF
-Player's Cards: #{display_hand(player)}
-  Player's Total: #{calculate_hand_value(player)}
-
-Dealer's Cards: #{display_initial_dealer(dealer)}
-  Dealer's Total: ???
+    Rounds Won: Player: #{score_board[:player]}  Dealer: #{score_board[:dealer]}
 
   EOF
 end
 
-def display_game(player, dealer, score_board)
+def display_game(player, dealer, score_board, display_dealer)
   display_heading(score_board)
-  puts <<-EOF
-Player's Cards: #{display_hand(player)}
-  Player's Total: #{calculate_hand_value(player)}
+  puts <<~EOF
+    Player's Cards: #{display_hand(player)}
+      Player's Total: #{calculate_hand_value(player)}
 
-Dealer's Cards: #{display_hand(dealer)}
-  Dealer's Total: #{calculate_hand_value(dealer)}
+    Dealer's Cards: #{display_dealer_cards(dealer, display_dealer)}
+      Dealer's Total: #{display_dealer_score(dealer, display_dealer)}
 
   EOF
 end
@@ -68,15 +56,19 @@ def display_hand(hand)
   end
 end
 
-def display_initial_dealer(hand, hidden = true)
+def display_dealer_cards(hand, hidden = true)
   hand.each_with_object("") do |card, cards_in_hand|
     if hidden
-      cards_in_hand << "| Unknown Card " if hidden
+      cards_in_hand << "| Unknown Card "
       hidden = false
     else
       cards_in_hand << "| #{card[0]} of #{card[1]}"
     end
   end
+end
+
+def display_dealer_score(hand, hidden = true)
+  hidden ? '????' : calculate_hand_value(hand)
 end
 
 def clear_screen
@@ -96,31 +88,30 @@ def calculate_hand_value(hand)
   values.each do |value|
     sum += card_value(value)
   end
-  ace_handling(values, sum)
-  sum
+  sum = ace_handling(values, sum)
 end
 
 def player_round(deck, score_board, player, dealer)
   loop do
-    break if busted?(player) || hit_blackjack(player)
+    break if busted?(player) || hit_blackjack?(player)
     puts "Would you like to hit or stay? (s) to stay or (h) to hit"
     break if gets.chomp.start_with?('s', 'S')
     player << deal_card(deck)
-    display_start_game(player, dealer, score_board)
+    display_game(player, dealer, score_board, true)
   end
 end
 
 def dealer_round(deck, score_board, player, dealer)
-  display_game(player, dealer, score_board)
+  display_game(player, dealer, score_board, false)
   puts "Dealer's turn"
   sleep 1
   loop do
-    break if busted?(dealer) || hit_blackjack(dealer)
+    break if busted?(dealer) || hit_blackjack?(dealer)
     if dealer_hit?(calculate_hand_value(dealer))
       puts "Dealer hits"
       sleep 2
       dealer << deal_card(deck)
-      display_game(player, dealer, score_board)
+      display_game(player, dealer, score_board, false)
     else
       puts "Dealer stays with #{calculate_hand_value(dealer)}"
       break
@@ -128,20 +119,26 @@ def dealer_round(deck, score_board, player, dealer)
   end
 end
 
+def dealt_blackjack?(player_cards)
+  calculate_hand_value(player_cards) == TOP_SCORE && player_cards.length == 2
+end
+
 def game_round(deck, score_board, player, dealer)
   loop do
     player_round(deck, score_board, player, dealer)
-    break if busted?(player)
+    break if busted?(player) || dealt_blackjack?(player)
     dealer_round(deck, score_board, player, dealer)
     break
   end
+  display_game_results(player, dealer)
+  increment_scoreboard(player, dealer, score_board)
 end
 
 def dealer_hit?(hand_value)
   hand_value < DEALER_STAYS
 end
 
-def hit_blackjack(hand)
+def hit_blackjack?(hand)
   calculate_hand_value(hand) == TOP_SCORE
 end
 
@@ -149,22 +146,50 @@ def busted?(hand)
   calculate_hand_value(hand) > TOP_SCORE
 end
 
-def calculate_winner(player_score, dealer_score, score_board)
-  case
-  when dealer_score > TOP_SCORE
-    puts "Dealer busted.  You win"
-    score_board[:player] += 1
-  when player_score > TOP_SCORE
-    puts "You busted.  Dealer wins"
-    score_board[:dealer] += 1
-  when player_score > dealer_score
-    puts "You won this round!"
-    score_board[:player] += 1
-  when player_score < dealer_score
-    puts "Dealer won this round"
-    score_board[:dealer] += 1
+def detect_results(player_cards, dealer_cards)
+  dealer_total = calculate_hand_value(dealer_cards)
+  player_total = calculate_hand_value(player_cards)
+  if player_total > TOP_SCORE
+    :player_busted
+  elsif dealer_total > TOP_SCORE
+    :dealer_busted
+  elsif player_total > dealer_total
+    :player_won
+  elsif player_total < dealer_total
+    :dealer_won
   else
-    puts "It was a tie, you split this round"
+    :tie
+  end
+end
+
+def increment_scoreboard(player_cards, dealer_cards, score_board)
+  results = detect_results(player_cards, dealer_cards)
+
+  case results
+  when :player_busted
+    score_board[:dealer] += 1
+  when :dealer_won
+    score_board[:dealer] += 1
+  when :dealer_busted
+    score_board[:player] += 1
+  when :player_won
+    score_board[:player] += 1
+  end
+end
+
+def display_game_results(player_cards, dealer_cards)
+  results = detect_results(player_cards, dealer_cards)
+  case results
+  when :dealer_busted
+    puts "Dealer busted.  You won"
+  when :player_busted
+    puts "You busted.  Dealer won"
+  when :player_won
+    puts "You won!"
+  when :dealer_won
+    puts "Dealer Won"
+  else
+    puts "It is a tie"
   end
 end
 
@@ -173,7 +198,7 @@ def ready_next_round
   gets.chomp
 end
 
-def check_for_winner(score_board)
+def check_for_winner?(score_board)
   score_board[:player] >= 5 || score_board[:dealer] >= 5
 end
 
@@ -202,17 +227,15 @@ loop do
       player_hand = initial_deal(game_deck)
       dealer_hand = initial_deal(game_deck)
 
-      display_start_game(player_hand, dealer_hand, score)
+      display_game(player_hand, dealer_hand, score, true)
 
       game_round(game_deck, score, player_hand, dealer_hand)
-      calculate_winner(calculate_hand_value(player_hand),
-                       calculate_hand_value(dealer_hand), score)
 
-      break if check_for_winner(score)
+      break if check_for_winner?(score)
       ready_next_round
     end
   end
-  display_game(player_hand, dealer_hand, score)
+  display_game(player_hand, dealer_hand, score, false)
   display_match_winner(score)
   break unless play_again?
 end
