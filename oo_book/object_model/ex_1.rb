ab44ff808167e3ae1f394cbda6d13bd5ae4319a2
Nick Dectis <ndectis@gmail.com>
2016-03-08 21:55:57 -0500

    rearange folders

--- oo_book/object_model/ex_1.rb
+++
@@ -1,9 +0,0 @@
-# How do we create an object in Ruby? Give an example of the creation of an object.
-
-# Objects are created by defining a class and using instantiation and the `new` class method.
-
-class TestClass
-
-end
-
-test_object = TestClass.new
