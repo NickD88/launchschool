9f6ac256b2863ed08e2e1e5a0c9156bf5795012b
Nick Dectis <ndectis@gmail.com>
2016-04-21 23:48:03 -0400

    finish anagram challenge

---
+++ challenges/anagram/anagram.rb
@@ -0,0 +1,26 @@
+class Anagram
+  attr_reader :initial_str, :sorted_str
+
+  def initialize(string)
+    @initial_str = string
+    @sorted_str = sort(string)
+  end
+
+  def match(array)
+    array.select { |word| anagram?(word) && different?(word) }
+  end
+
+  private
+
+  def sort(string)
+    string.downcase.chars.sort.join('')
+  end
+
+  def different?(string)
+    initial_str.downcase != string.downcase
+  end
+
+  def anagram?(test_str)
+    sorted_str == sort(test_str)
+  end
+end
